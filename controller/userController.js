f52f66636fb3e91e6a5ba24d6965284229e03a6d
Goodluck <84187549+Goodluck-dc14@users.noreply.github.com>
2024-09-05 13:01:35 +0100

    update

--- controller/userController.js
+++ controller/userController.js
@@ -120,3 +120,120 @@ exports.runAiModel = async (req, res, next) => {
         return next(error);
     }
 };
+exports.uploadDocument = async (req, res, next) => {
+    try {
+        const id = req.params.userId;
+        const user = await userModel.findById(id);
+
+        // if (!user) return res.status(404).json({ message: "User does not exist" });
+        if (!user) throw new AppError(404, "User does not exist");
+
+        const files = req.file;
+        if (!files) return res.status.json({ message: "File is required" });
+
+        const imageURL = await cloudinary.uploads(files.path);
+
+        const uploadFile = await userModel.findByIdAndUpdate(user._id, { uploadedCV: imageURL.url }, { new: true });
+
+        res.status(200).json({
+            status: "Successful",
+            data: uploadFile
+        });
+
+    } catch (error) {
+        return next(error);
+    }
+};
+
+exports.getSingleUser = async (req, res, next) => {
+    try {
+        const id = req.params.userId;
+        const user = await userModel.findById(id);
+
+        if (!user) throw new AppError(404, "User does not exist");
+
+        res.status(200).json({
+            status: "Successful",
+            data: user
+        });
+
+    } catch (error) {
+        return next(error);
+    }
+};
+exports.getAllUsers = async (req, res, next) => {
+    try {
+        const user = await userModel.find();
+        if (user < 1) throw new AppError(404, "NO User Found");
+
+        res.status(200).json({
+            status: "Successful",
+            data: user
+        });
+
+    } catch (error) {
+        return next(error);
+    }
+};
+
+exports.runAiModel = async (req, res, next) => {
+    try {
+        const id = req.params.userId;
+        const user = await userModel.findById(id);
+        if (!user) throw new AppError(404, "User does not exist");
+
+        const response = await axios.post('https://soma-model.onrender.com/match-scholarships', user);
+
+        const result = response.data;
+        // console.log(result);
+        // if (Array.isArray(result)) {
+        //     for (let data of result) {
+        //         const Airesponse = await AiResponseSchema(data);
+        //         Airesponse.user = user._id;
+        //         Airesponse.save();
+        //         user.ResponseAi?.push(new mongoose.Types.ObjectId(Airesponse._id));
+        //     };
+        //     // user.save();
+        // } else {
+        //     console.error("Response data is not an array");
+        // }
+
+        // user.save();
+
+        // const Airesponse = await AiResponseSchema({
+        //     aiRes: response.data
+        // });
+
+        // Airesponse.user = user._id;
+        // Airesponse.save();
+        user.ResponseAi = result;
+        user.save();
+
+        res.status(200).json({
+            message: "Data response as been added to the database",
+            data: user
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+exports.use_external_user_to_runAiModel = async (req, res, next) => {
+    try {
+        const user = await userModel(req.body);
+
+        const response = await axios.post('https://soma-model.onrender.com/match-scholarships', user);
+
+        const result = response.data;
+        user.ResponseAi = result;
+        user.save();
+
+        res.status(200).json({
+            message: "Data response as been added to the database",
+            data: user
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
+
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { updateUser, uploadDocument, getAllUsers, getSingleUser, runAiModel } = require('../controller/userController');
+const { updateUser, uploadDocument, getAllUsers, getSingleUser, runAiModel, use_external_user_to_runAiModel } = require('../controller/userController');
 const router = express.Router();
 const upload = require('../utils/multer');
 
 router.route('/update').patch(updateUser);
 router.route('/').get(getAllUsers);
+router.route('/ai-model').post(use_external_user_to_runAiModel);
 router.route('/:userId').get(getSingleUser);
 router.route('/:userId').post(runAiModel);
 router.route('/:userId/upload').patch(upload.single('uploadedCV'), uploadDocument);
